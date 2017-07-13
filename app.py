543410da7c4f72fa115fad2b8cc6405ff344ee8c
Rohith Pentaparthy <penta@umich.edu>
2017-07-13 17:18:59 -0400

    added a table for the .csv data

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
+import csv
 from flask import Flask 
 from flask import render_template
 app = Flask(__name__)
 
 
+def get_csv():
+    csv_path = './static/la-riots-deaths.csv'  #path to file
+    csv_file = open(csv_path, 'rb')            #open the csv
+    csv_obj = csv.DictReader(csv_file)         #parse as dict
+    csv_list = list(csv_obj)                   #keeps csv object permanently
+    return csv_list
+
 
 @app.route('/')
 def index():
     template = 'index.html'
-    return render_template(template)
+    object_list = get_csv()
+    return render_template(template, object_list = object_list)
 
 if __name__ == '__main__':
     app.run(use_reloader=True, debug=True)
